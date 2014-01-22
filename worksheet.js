5806a3a6a78ef69f14d4c79b45ebd096858ff215
James White <james@llamasantos.com>
2014-01-21 21:01:42 -0700

    Added test files

--- worksheet.js
+++ worksheet.js
@@ -1,21 +1,5 @@
 var _ = require('underscore');
 
-function Ticker(done){
-	var counter = 0;
-	return {
-		add : function (len){
-			counter += len;
-		},
-		done : function (){
-			counter--;
-
-			if (counter === 0)
-				done();
-		}
-	};
-};
-
-
 var tick = Ticker(function (){
 	console.info('Finished');
 });
